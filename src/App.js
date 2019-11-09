edd70f6757d9ca13ff21ec2d58b1471a7d10ab14
Brendan Goudy <brendan.p.goudy@gmail.com>
2019-11-08 22:07:39 -0500

    Correcting file structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Score from "./components/ScoreBar";
+import Jumbotron from "./components/PicWindow";
+import Pictures from "";
+
+
+class App extends Component {
+
+  state = 
+  {
+    pictures,
+    score: 0,
+    highScore: 0,
+    picturesClicked: [],
+    text: "Hey There! Try to pick all of the actors head shots without picking the same one twice. Pick a Pic to get going."
+  };
+
+  
+}
